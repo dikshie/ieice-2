2c456c716e751f1fbc7947c7d1f06ff6fa8ff7db
dikshie <dikshie@gmail.com>
2013-02-18 01:02:49 +0900

    bulk add

---
+++ script.sh
@@ -0,0 +1,5 @@
+/usr/local/texlive/2012/bin/x86_64-darwin/bibtex jurnal
+/usr/local/texlive/2012/bin/x86_64-darwin/latex jurnal 
+/usr/local/texlive/2012/bin/x86_64-darwin/latex jurnal 
+/usr/local/texlive/2012/bin/x86_64-darwin/dvips -t a4 -G0 -o jurnal.ps jurnal.dvi
+ps2pdf -sPAPERSIZE=a4 -dMaxSubsetPct=100 -dCompatibilityLevel=1.3 -dSubsetFonts=true -dEmbedAllFonts=true -dAutoFilterColorImages=false -dAutoFilterGrayImages=false  -dColorImageFilter=/FlateEncode -dGrayImageFilter=/FlateEncode -dMonoImageFilter=/FlateEncode jurnal.ps jurnal.pdf
